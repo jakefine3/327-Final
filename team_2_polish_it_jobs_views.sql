fc9b06f8bf578c3c25aed36978d03138d818ef4b
michaelmurphy22 <165197976+michaelmurphy22@users.noreply.github.com>
2024-05-08 16:19:40 -0400

    Update team_2_polish_it_jobs_views.sql

--- team_2_polish_it_jobs_views.sql
+++ team_2_polish_it_jobs_views.sql
@@ -24,3 +24,34 @@ WHERE if_permanent = 'TRUE' AND experience_level = 'senior';
 SELECT * FROM permanent_senior_jobs;
 
 /* View 3 */
+CREATE VIEW remote_salary_average AS
+SELECT j.if_remote, AVG(s.salary_from) AS "Average Salary Minimum", AVG(s.salary_to) AS "Average Salary Maximum"
+FROM job j
+JOIN salary s ON j.salary_id = s.salary_id
+WHERE j.if_remote = "TRUE"
+GROUP BY j.if_remote;
+
+/*View 4*/
+CREATE VIEW high_skill_jobs AS 
+SELECT j.job_id, t.title_name, t.marker_icon, s.skill_id, SUM(s.skill_value_1 + s.skill_value_2 + s.skill_value_3) AS total_skill_value
+FROM job j
+JOIN title t ON j.title_id = t.title_id
+JOIN skills s ON j.skill_id = s.skill_id
+GROUP BY j.job_id, t.title_name, t.marker_icon, s.skill_id
+HAVING SUM(s.skill_value_1 + s.skill_value_2 + s.skill_value_3) > 10;
+
+SELECT * FROM high_skill_jobs;
+
+/*View 5*/
+CREATE VIEW top_salaries AS
+SELECT j.job_id, t.title_name, s.salary_to AS "Max Salaries", s.currency
+FROM job j
+JOIN title t ON j.title_id = t.title_id
+JOIN salary s ON j.salary_id = s.salary_id
+WHERE s.salary_id IN (
+    SELECT salary_id
+    FROM salary
+    GROUP BY salary_id
+    ORDER BY MAX(salary_to) DESC
+);
+
